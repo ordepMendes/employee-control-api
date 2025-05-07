11f2a04e74b2b2b3dd429eb3b7ed3785b42dc064
Pedro Mendes <pedrohh0012@gmail.com>
2025-05-07 14:05:57 -0300

    feat: :sparkles: GET de listagem de funcionarios implementado
    Foi implementado o get geral dos funcionarios ja funcional

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
 import { FastifyRequest, FastifyPluginOptions, FastifyInstance, FastifyReply } from "fastify";
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomersController } from "./controllers/ListCustomersController";
+
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
-    fastify.get("/employee-list", async (request: FastifyRequest, reply: FastifyReply) =>{
-        return {
-            nome: "khazix",
-            idade: 1245
-        }
-    })
 
     fastify.post("/add-employee", async(request: FastifyRequest, reply: FastifyReply) => {
         return new CreateCustomerController().handle(request, reply);
+    })
+
+    fastify.get("/list-employees", async(request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCustomersController().handle(request, reply);
     })
 }
