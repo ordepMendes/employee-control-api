c3075c209fdec41357bc04763b82016d57f2796a
Pedro Mendes <pedrohh0012@gmail.com>
2025-05-07 23:57:51 -0300

    fix: :bug: Liberação de cors

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,26 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { routes } from "./routes";
 
-const app = Fastify({logger: true});
+const app = Fastify({ logger: true });
 
 app.setErrorHandler((error, request, reply) => {
-    reply.code(400).send({message: error.message});
-})
+  reply.code(400).send({ message: error.message });
+});
 
 const start = async () => {
-    await app.register(cors);
-    await app.register(routes);
+  await app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  });
 
-    try {
-        const port = Number(process.env.PORT) || 3333;
-        await app.listen({ port, host: '0.0.0.0' });
-    } catch (error) {
-        process.exit(1);
-    }
-}
+  await app.register(routes);
+
+  try {
+    const port = Number(process.env.PORT) || 3333;
+    await app.listen({ port, host: "0.0.0.0" });
+  } catch (error) {
+    process.exit(1);
+  }
+};
 
 start();
